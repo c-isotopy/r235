7ff5577c7002b09c2a377503107de8ac826e992d
Chang Glasgow <czglas@gmail.com>
2022-02-11 22:12:19 -0500

    Requirement 4 - deny negatives.

--- R365.Services/Evaluation/ExpressionEvaluationService.cs
+++ R365.Services/Evaluation/ExpressionEvaluationService.cs
@@ -1,4 +1,5 @@
 ﻿using R365.Models;
+using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -12,6 +13,14 @@ namespace R365.Services.Evaluation
             ct.ThrowIfCancellationRequested();
 
             var components = expression?.Components;
+
+            if (components != null)
+            {
+                var negatives = components.Where(candidate => candidate < 0).ToArray();
+                if (negatives.Length > 0)
+                    throw new ArgumentOutOfRangeException($"Invalid inputs - negative values are not allowed: ({string.Join(", ", negatives)})"); 
+            }
+
             var result = components == null ? 0 : components.Sum();
 
             return Task.FromResult(result);
--- R365.Test/EvaluationService_Tests.cs
+++ R365.Test/EvaluationService_Tests.cs
@@ -75,7 +75,7 @@ namespace R365.Test
         {
             #region Data
             var random = new Random();
-            var values = new int[] { random.Next(-2000, 2000), random.Next(-2000, 2000) };
+            var values = new int[] { random.Next(0, 2000), random.Next(0, 2000) };
             var expressionDto = new ExpressionDto(values);
             #endregion
 
@@ -89,6 +89,38 @@ namespace R365.Test
 
             #region Assertions
             Assert.AreEqual(values[0] + values[1], actualResult);
+            #endregion
+        }
+
+        [TestMethod]
+        public void EvaluateAsync_expression_of_negatives_throws()
+        {
+            #region Data
+            var random = new Random();
+            var values = new int[] { random.Next(-2000, 0), random.Next(-2000, 0), random.Next(-2000, 0) };
+            var expressionDto = new ExpressionDto(values);
+            #endregion
+
+            #region Setup
+            #endregion
+
+            #region Execution
+            Exception raisedException = null;
+            try
+            {
+                var service = new ExpressionEvaluationService();
+                var actualResult = service.EvaluateAsync(expressionDto, new CancellationToken()).GetAwaiter().GetResult();
+            }
+            catch (Exception e)
+            {
+                raisedException = e;
+            }
+            #endregion
+
+            #region Assertions
+            Assert.IsNotNull(raisedException);
+            foreach (var value in values)
+                Assert.IsTrue(raisedException.Message.Contains(value.ToString()));
             #endregion
         }
     }
